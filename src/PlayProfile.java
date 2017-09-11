6eb074e903644d4b5013c0b86159e4aa5032301d
986136 <yozhang@mum.edu>
2017-09-11 11:38:49 -0500

    add player and player profile class

--- src/PlayProfile.java
+++ src/PlayProfile.java
@@ -1,9 +1,45 @@
 public class PlayProfile {
     private int id;
     private String name;
-    private int score;
     private double winRate;
-    private int rank;
+    private int eloResult;
 
+    public PlayProfile(int id, String name, double winRate, int eloResult) {
+        this.id = id;
+        this.name = name;
+        this.winRate = winRate;
+        this.eloResult = eloResult;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWinRate() {
+        return winRate;
+    }
+
+    public void setWinRate(double winRate) {
+        this.winRate = winRate;
+    }
+
+    public int getEloResult() {
+        return eloResult;
+    }
+
+    public void setEloResult(int eloResult) {
+        this.eloResult = eloResult;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,20 @@
 public class Player {
     public int remainderTime;  //second
+    PlayProfile playProfile;
+
+    public int getRemainderTime() {
+        return remainderTime;
+    }
+
+    public void setRemainderTime(int remainderTime) {
+        this.remainderTime = remainderTime;
+    }
+
+    public PlayProfile getPlayProfile() {
+        return playProfile;
+    }
+
+    public void setPlayProfile(PlayProfile playProfile) {
+        this.playProfile = playProfile;
+    }
 }
