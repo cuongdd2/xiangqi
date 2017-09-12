cd7cc2f3034b8abe8bb0816893095f83cfc79cb7
Yongzhao Zhang <yozhang@mum.edu>
2017-09-11 21:17:19 -0500

    add advisor

--- src/game/Chariot.java
+++ src/game/Chariot.java
@@ -2,10 +2,26 @@ package game;
 
 import javafx.scene.image.Image;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Chariot extends Piece {
     public Chariot(boolean black) {
         this.black = black;
         String url = (black ? "black" : "red") + "/r.png";
         this.setImage(new Image(url));
     }
+
+    public List<P> getMovable(){
+        List<P> list = new ArrayList<>();
+        for (int x = 0; x <= Val.MaxX;x++){
+            for (int y=0;y<Val.MaxY;y++){
+                list.add(new P(x,y));
+            }
+        }
+
+        return list;
+    }
+
+
 }
