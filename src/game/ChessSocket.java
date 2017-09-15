d61075a076908aa1c3379be0978e68a0f30bbdec
CuongDD <cuongdd2@gmail.com>
2017-09-15 16:20:28 -0500

    change ip host to local

--- src/game/ChessSocket.java
+++ src/game/ChessSocket.java
@@ -7,7 +7,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ChessSocket {
-    private static final String Host = "138.197.92.4";
+//    private static final String Host = "138.197.92.4";
+    private static final String Host = "10.10.13.86";
     private Socket s = new Socket(Host, 14120);
     private PrintWriter out = new PrintWriter(s.getOutputStream(), true);
     BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
